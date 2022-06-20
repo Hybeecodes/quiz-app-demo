383edec9db9eb1941c8b0966c31123fc2f7d122a
hybeecodes <obikoya11@gmail.com>
2022-06-20 19:12:38 +0100

    first commit

---
+++ js/question.js
@@ -0,0 +1,53 @@
+const questions = [
+  {
+    id: 1,
+    title: "What is the name of the main character in the game?",
+    A: "Mario",
+    B: "Luigi",
+    C: "Peach",
+    D: "Toad",
+    answer: "A",
+    text: "First Question"
+  },
+  {
+    id: 2,
+    title: "Who is the President of the United States?",
+    A: "Donald Trump",
+    B: "Barack Obama",
+    C: "George W. Bush",
+    D: "Bill Clinton",
+    answer: "C",
+    text: "Second Question"
+  },
+  {
+    id: 3,
+    title: "What is the capital of the United States?",
+    A: "Washington D.C.",
+    B: "New York",
+    C: "Los Angeles",
+    D: "Chicago",
+    answer: "A",
+    text: "Third Question"
+  },
+
+  {
+    id: 4,
+    title: "Which is the largest country in the world?",
+    A: "China",
+    B: "Russia",
+    C: "India",
+    D: "United States",
+    answer: "D",
+    text: "Fourth Question"
+  },
+  {
+    id: 5,
+    title: "What does HTML stand for?",
+    A: "Hyper Text Markup Language",
+    B: "High Text Markup Language",
+    C: "HiHat Text Markup Language",
+    D: "Sorry, I don't know that one",
+    answer: "A",
+    text: "Fifth Question"
+  }
+];
